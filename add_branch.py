1ef92e373184db94ee165e2f576db795869b458c
choiwonkyu <wk.choi@thehyoosik.com>
2025-04-05 17:54:01 +0900

    branche list 추가

--- add_branch.py
+++ add_branch.py
@@ -1,13 +1,11 @@
 import sqlite3
 
 def add_branch(name, phone, chat_url, slug):
-    try:
-        with get_db_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute('''
-                INSERT INTO branches (name, phone, chat_url, slug)
-                VALUES (?, ?, ?, ?)
-            ''', (name, phone, chat_url, slug))
-            conn.commit()
-    except Exception as e:
-        print("DB 오류:", e)
+    conn = sqlite3.connect('branches.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO branches (name, phone, chat_url, slug)
+        VALUES (?, ?, ?, ?)
+    ''', (name, phone, chat_url, slug))
+    conn.commit()
+    conn.close()
